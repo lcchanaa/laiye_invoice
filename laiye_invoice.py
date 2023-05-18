dd6d167964cc98d3f8e4b4ceb6c65620feaa45bd
lcchanaa <123953682+lcchanaa@users.noreply.github.com>
2023-05-18 10:57:10 +0800

    Update laiye_invoice.py

--- laiye_invoice.py
+++ laiye_invoice.py
@@ -9,7 +9,7 @@ invoice_date = col1.text_input("Invoice Date", key="invoice_date")
 sold_to = col1.text_input("Sold To", key="sold_to")
 ship_to = col1.text_input("Ship To", key="ship_to")
 po_no = col1.text_input("PO Number", key="po_no")
-payment_terms = col1.selectbox("Payment Terms", ("<Please choose>", "30 Days Net", "45 Days Net", "60 Days Net", "Others"), key="payment_terms")
+payment_terms = col1.radio("Payment Terms", ("30 Days Net", "45 Days Net", "60 Days Net", "Others"), key="payment_terms")
 due_date = col1.text_input("Due Date", key="due_date")
 total_amt = col1.number_input("Total Amount", key="total_amt")
 
